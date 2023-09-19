5d92f28e6e79fbae1cffbbde59be197436856439
Fatin110 <ishmam110@gmail.com>
2023-09-19 22:09:11 +0600

    task3 added

--- task3.php
+++ task3.php
@@ -1,1 +1,27 @@
 <?php 
+//printing first 10 fibonacci numbers
+function fibonacci($n)
+ {
+ 
+  $first = 0;
+  $second = 1;
+ 
+  echo $first.' '.$second.' ';
+ 
+  for($i = 2; $i < $n; $i++){
+
+    $third = $first + $second;
+
+    if ($first > 100 || $second > 100 || $third > 100) {
+        break;
+    }
+ 
+    echo $third.' ';
+ 
+    $first = $second;
+    $second = $third;
+ 
+    }
+}
+
+fibonacci(10);
